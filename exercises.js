7622d5631c934004b88f4f7e0082689ff6d896e0
Paula Suarez <paula.suarez@freeman.com>
2020-06-27 10:59:56 -0500

    Adding updates to repo

---
+++ exercises.js
@@ -0,0 +1,30 @@
+//types
+var myString;
+var myNum;
+var myBool;
+var num;
+var mySlice;
+myString = "Hey, hey string!";
+mySlice = "Hello".slice(0, 3);
+myNum = 25;
+myBool = true;
+num = "hello friend";
+console.log(mySlice);
+//array types
+var strArr;
+var numArr;
+var boolArr;
+strArr = ["hello", "la monde"];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+console.log(boolArr);
+//array with angle brackets
+var stringArray;
+var numberArray;
+var booleanArray;
+stringArray = ["this", "is so fun!"];
+console.log(stringArray);
+//tuple
+var strNumTuple;
+strNumTuple = ["hi friends", 25];
+console.log(strNumTuple);
---
+++ exercises.ts
@@ -0,0 +1,49 @@
+//types
+
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let num: any;
+let mySlice: string;
+
+myString = "Hey, hey string!";
+mySlice = "Hello".slice (0, 3);
+myNum = 25;
+myBool = true;
+num = "hello friend";
+
+console.log(mySlice);
+
+
+//array types
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+strArr = ["hello", "la monde"];
+numArr = [1,2,3];
+boolArr = [true, false, true];
+
+console.log(boolArr);
+
+//array with angle brackets
+
+let stringArray: Array<string>;
+let numberArray: Array<number>;
+let booleanArray: Array<boolean>;
+
+stringArray = ["this", "is so fun!"];
+
+console.log (stringArray);
+
+//tuple
+
+let strNumTuple: [string, number];
+
+strNumTuple = ["hi friends", 25];
+
+console.log(strNumTuple);
+
+
+
